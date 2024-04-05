b0b1f5cbaa696f0251954bb912055e6219a4bc0f
TaniuOp <taniuop@gmail.com>
2024-04-05 16:06:28 +0200

    footer info added and layout fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './Footer.css';
+import Logo from '../../assets/akomegaag_logo.png';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="footer-info">
-        <p>Company Name</p>
-        <p>Address: 123 Street, City, Country</p>
+    <footer className='footer'>
+      <div className='footer-logo'>
+        <img src={Logo} alt='Logo' />
       </div>
-      <div className="footer-social">
-        <a href="#"><i className="fa fa-facebook"></i></a>
-        <a href="#"><i className="fa fa-twitter"></i></a>
-        <a href="#"><i className="fa fa-instagram"></i></a>
+      <div className='footer-info'>
+        <p>AK Omega GU AG</p>
+        <p>Hauptstrasse 69</p>
+        <p>5734 Reinach</p>
+      </div>
+      <div className='footer-info'>
+        <p>Diana Heger</p>
+        <a href='#'>
+          <i className='fa fa-twitter'>E-Mail: info@omega-gu-ag.ch</i>
+        </a>
+        <p className='fa fa-twptter'>Telefon: 062 771 50 07</p>
+      </div>
+      <div className='footer-info'>
+        <p> Öffnungszeiten: </p>
+        <p> Montag - Freitag </p>
+        <p> 08:00 - 12:00 & 13:30 - 17:00</p>
       </div>
     </footer>
   );
