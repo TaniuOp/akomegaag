8de05c58e0634d3ad8502cf1c09a2fc5b00f904a
TaniuOp <taniuop@gmail.com>
2024-04-08 10:42:27 +0200

    add project component

---
+++ src/components/Home/Projects/Projecsts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects: React.FC = () => {
+  return (
+    <div className='projects-container'>
+      <p>PROJECTS</p>
+    </div>
+  );
+};
+
+export default Projects;
