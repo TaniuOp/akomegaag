2a9c40586ad44d8e1e975d17023c48fee90cf4ec
TaniuOp <taniuop@gmail.com>
2024-04-13 17:33:26 +0200

    home color fix

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -35,7 +35,7 @@ const Home: React.FC = () => {
       </header>
 
       <main>
-        <section className='projects'>
+        <section className='concept-section'>
           <p>
             Wir sind ein Architekturbüro, dass sich durch sein soziales, ökologisches und
             kulturelles Bewusstsein auszeichnet.
@@ -45,35 +45,38 @@ const Home: React.FC = () => {
             Umweltinnovationen beinhalten, bieten wir Ihnen die bestmögliche Lösung für Ihr Projekt.
           </p>
         </section>
-        <section className='team'>
-          <h2>ÜBER UNS</h2>
-          <p>
-            AK Omega AG ist ein Bauunternehmen mit mehr als 10 Jahren Erfahrung, das sich auf den
-            Wohnungsbau spezialisiert hat. AK Architekten AG ist ein junges Unternehmen mit
-            Anerkennung im Kanton Aargau, zusammen haben sie mehr als 27.000 Quadratmeter Bau
-            entwickelt.
-          </p>
-
-          <p>
-            Das Team arbeitet unter der Leitung der Architekturabteilung, die von der Architektin
-            Diana Heger geführt wird. Der Leiter der Bauabteilung ist Stefan Schüpfer. Die
-            administrative und immobilienwirtschaftliche Projektleitung liegt bei Ivan J.
-          </p>
-
-          <p>
-            Darüber hinaus verfügen wir über ein professionelles Team von Spezialisten in jedem
-            Arbeitsbereich (Baumeister, Sanitär, Elektro, Landschaftsbau...) für die Ausführung und
-            ganzheitliche Entwicklung unserer Projekte.
-          </p>
-        </section>
-        <section>
-          <Services />
-        </section>
 
         <div className='projects-container'>
           <h2> PROJEKTE</h2>
           <Projects />
         </div>
+
+        <section className='team'>
+          <h2>ÜBER UNS</h2>
+          <div className={'about-section'}>
+            <p>
+              AK Omega AG ist ein Bauunternehmen mit mehr als 10 Jahren Erfahrung, das sich auf den
+              Wohnungsbau spezialisiert hat. AK Architekten AG ist ein junges Unternehmen mit
+              Anerkennung im Kanton Aargau, zusammen haben sie mehr als 27.000 Quadratmeter Bau
+              entwickelt.
+            </p>
+
+            <p>
+              Das Team arbeitet unter der Leitung der Architekturabteilung, die von der Architektin
+              Diana Heger geführt wird. Der Leiter der Bauabteilung ist Stefan Schüpfer. Die
+              administrative und immobilienwirtschaftliche Projektleitung liegt bei Ivan J.
+            </p>
+
+            <p>
+              Darüber hinaus verfügen wir über ein professionelles Team von Spezialisten in jedem
+              Arbeitsbereich (Baumeister, Sanitär, Elektro, Landschaftsbau...) für die Ausführung
+              und ganzheitliche Entwicklung unserer Projekte.
+            </p>
+          </div>
+        </section>
+        <section>
+          <Services />
+        </section>
 
         <section className='contact-container'>
           <h2> KONTAKTIERE UNS </h2>
