48d9173bea27dff8852998b7f95ddbb988e225cc
TaniuOp <taniuop@gmail.com>
2024-04-13 17:33:36 +0200

    header fixes to hide menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,30 +3,38 @@ import './Header.css';
 import Logo from '../../assets/akomegaag_logo.png';
 
 const Header: React.FC = () => {
-    const [showMenu, setShowMenu] = useState(false);
+  //   const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
+  //   const toggleMenu = () => {
+  //     setShowMenu(!showMenu);
+  //   };
 
-    return (
-        <header className="header">
-            <div className="header-logo">
-                <img src={Logo} alt="Logo" />
-            </div>
-            <nav className={`header-nav ${showMenu ? 'show-menu' : ''}`}>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/projects">Projects</a></li>
-                    <li><a href="/team">Team</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
-            <button className="header-menu-toggle" onClick={toggleMenu} style={{ display: window.innerWidth <= 768  ? 'block' : 'none' }}>
+  return (
+    <header className='header'>
+      <div className='header-logo'>
+        <img src={Logo} alt='Logo' />
+      </div>
+      {/* <nav className={`header-nav ${showMenu ? 'show-menu' : ''}`}>
+        <ul>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/projects'>Projects</a>
+          </li>
+          <li>
+            <a href='/team'>Team</a>
+          </li>
+          <li>
+            <a href='/contact'>Contact</a>
+          </li>
+        </ul>
+      </nav> */}
+      {/* <button className="header-menu-toggle" onClick={toggleMenu} style={{ display: window.innerWidth <= 768  ? 'block' : 'none' }}>
                 &#9776;
-            </button>
-        </header>
-    );
+            </button> */}
+    </header>
+  );
 };
 
 export default Header;
