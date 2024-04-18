16ddda28a8e2dc9dddbf5bca3c0c7cf76fb9c5a6
TaniuBM <taniuop@gmail.com>
2024-04-18 22:23:07 +0200

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   base: '/akomegaag',
 });
