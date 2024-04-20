c3057e0d24c5a02bc81fb793785cc6f7dca23af8
TaniuBM <taniuop@gmail.com>
2024-04-20 15:03:46 +0200

    test again with js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var client_1 = require("react-dom/client");
-var App_tsx_1 = require("./App.tsx");
-require("./index.css");
-client_1["default"].createRoot(document.getElementById('root')).render(<react_1["default"].StrictMode>
-    <App_tsx_1["default"] />
-  </react_1["default"].StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
